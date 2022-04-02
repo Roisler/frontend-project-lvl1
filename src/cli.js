01ca43ca2a0fd6cb4f2a295c4860104683ca6fd0
roisler <roislerxs@gmail.com>
2022-04-02 21:34:27 +0700

    four step: CodeClimate, eslint, Action

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const nameUser = () => {
-  const writeName = readlineSync.question("May I have your name? ");
-  console.log(`${"Hello,"} ${writeName}!`);
+const nameUser = () => {
+  const writeName = readlineSync.question('May I have your name? ');
+  console.log(`${'Hello,'} ${writeName}!`);
 };
+
+export default nameUser;
