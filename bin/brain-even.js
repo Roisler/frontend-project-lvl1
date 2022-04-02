a05bf6ecd7ef483831aa6de938a141a7fe40fc9f
roisler <roislerxs@gmail.com>
2022-04-03 00:16:21 +0700

    five step, add brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const writeName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${writeName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+for (let i = 0; i < 3; i += 1) {
+    let randomNum = Math.floor(Math.random() * 100) + 1;
+    let newAnswer = readlineSync.question(`Question: ${randomNum}\nYour Answer: `);
+    let correctAnswer = (randomNum % 2 === 0) ? 'yes' : 'no';
+    if (newAnswer !== correctAnswer) {
+        console.log(`'${newAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+        console.log(`Let's try again, ${writeName}!`);
+        break;
+    } else {
+        console.log('Correct!');
+    };
+    if (i === 2 && newAnswer === correctAnswer) {
+        console.log(`Congratulations, ${writeName}!`);
+    };
+};
