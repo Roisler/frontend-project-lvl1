5b868c17e37a0425748a3c6c67e6a0d10c7aa701
roisler <roislerxs@gmail.com>
2022-04-05 21:52:40 +0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import readlineSync from 'readline-sync';
 export const hello = () => console.log('Welcome to the Brain Games!');
 export const nameUser = () => {
   const writeName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${writeName}!`);
   return writeName;
 };
 
 export const newAnswer = (mainQuestion) => {
-  const answer = readlineSync.question(`${mainQuestion}`);
-  return (answer);
+  const answer = readlineSync.question(`Question: ${mainQuestion}\nYour answer: `);
+  return (Number(answer));
 };
 export const randomNum = () => Math.floor(Math.random() * 20) + 1;
 export const randomElement = (arr) => {
