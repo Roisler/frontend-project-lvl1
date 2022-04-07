dbd70e75a5e7d8c34de950ec868120ac8f55f752
roisler <roislerxs@gmail.com>
2022-04-07 21:56:27 +0700

    nine step

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export const hello = () => console.log('Welcome to the Brain Games!');
+
 export const nameUser = () => {
   const writeName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${writeName}!`);
@@ -9,9 +10,11 @@ export const nameUser = () => {
 
 export const newAnswer = (mainQuestion) => {
   const answer = readlineSync.question(`Question: ${mainQuestion}\nYour answer: `);
-  return (Number(answer));
+  return answer;
 };
+
 export const randomNum = (max) => Math.floor(Math.random() * max) + 1;
+
 export const randomElement = (arr) => {
   const i = Math.floor(Math.random() * arr.length);
   return arr[i];
