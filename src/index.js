fad8dff45a9ca3bdb8a57f831d3e58035b8b5a37
roisler <roislerxs@gmail.com>
2022-04-09 00:35:22 +0700

    refactoring code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import readlineSync from 'readline-sync';
 
-export const hello = () => console.log('Welcome to the Brain Games!');
+export const runGame = (task, getAnswerAndQuestion) => {
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+  console.log(task);
+  for (let rounds = 0; rounds < 3; rounds += 1) {
+    const [stepQuestion, correctAnswer] = getAnswerAndQuestion();
+    const answer = readlineSync.question(`Question: ${stepQuestion}\nYour answer: `);
 
-export const nameUser = () => {
-  const writeName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${writeName}!`);
-  return writeName;
-};
-
-export const newAnswer = (mainQuestion) => {
-  const answer = readlineSync.question(`Question: ${mainQuestion}\nYour answer: `);
-  return answer;
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${nameUser}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${nameUser}!`);
 };
 
 export const randomNum = (max) => Math.floor(Math.random() * max) + 1;
