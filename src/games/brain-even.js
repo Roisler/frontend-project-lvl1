b5948981023b44fafbca0c392218c0ae10d5cd67
roisler <roislerxs@gmail.com>
2022-04-11 23:28:44 +0700

    fix isEven, fix import

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,13 +1,19 @@
-import * as question from '../index.js';
+import launch from '../index.js';
+import { getRandomIntInclusive } from '../utils.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getAnswerAndQUestion = () => {
-  const stepQuestion = question.randomNum(1000);
-  const correctAnswer = stepQuestion % 2 === 0 ? 'yes' : 'no';
+const isEven = (num) => {
+  const even = (num % 2 === 0) ? 'yes' : 'no';
+  return even;
+};
+
+const getAnswerAndQuestion = () => {
+  const stepQuestion = getRandomIntInclusive(0, 1000);
+  const correctAnswer = isEven(stepQuestion);
   return [stepQuestion, correctAnswer];
 };
 
-const runBrainEven = () => question.runGame(task, getAnswerAndQUestion);
+const runBrainEven = () => launch(task, getAnswerAndQuestion);
 
 export default runBrainEven;
