f55dad4d3aa6a8b74b0ed186025524ed22a88995
roisler <roislerxs@gmail.com>
2022-04-11 23:26:57 +0700

    rename variables, fix magic numbers, remove getRandom function

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
 import readlineSync from 'readline-sync';
 
-export const runGame = (task, getAnswerAndQuestion) => {
+export default (task, getAnswerAndQuestion) => {
   console.log('Welcome to the Brain Games!');
   const nameUser = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nameUser}!`);
   console.log(task);
-  for (let rounds = 0; rounds < 3; rounds += 1) {
+  const countOfRound = 3;
+  for (let round = 0; round < countOfRound; round += 1) {
     const [stepQuestion, correctAnswer] = getAnswerAndQuestion();
-    const answer = readlineSync.question(`Question: ${stepQuestion}\nYour answer: `);
+    console.log(`Question: ${stepQuestion}`);
+    const answer = readlineSync.question('Your answer: ');
 
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (answer !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
       console.log(`Let's try again, ${nameUser}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${nameUser}!`);
-};
-
-export const randomNum = (max) => Math.floor(Math.random() * max) + 1;
-
-export const randomElement = (arr) => {
-  const i = Math.floor(Math.random() * arr.length);
-  return arr[i];
 };
